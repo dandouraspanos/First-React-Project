469469396e27a934fa0a28d333725886beec300c
Panos Dandouras <dandouraspanos@gmail.com>
2026-04-15 11:11:20 -0400

    we moved the components to their own folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
-function Header() {
-  const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-  function genRandomInt (max){
-  return Math.floor(Math.random() * (max + 1));
-}
-const description = reactDescriptions[genRandomInt(2)];
-return (
-  <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-    <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
+// function UserData(){
+//   const user = {first:"George",last:"Salayka", age:"40" };
 
+//   return (
+//     <div>
+//       <h2>Name: {user.last}, {user.first}</h2>
+//       <h3>Age: {user.age}</h3>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[0]
+              }
+            />
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[1]
+              }
+            />
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[2]
+              }
+            />
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[3]
+              }
+            />
+            {
+              // CORE_CONCEPTS.map((concept)=><CoreConcept {...concept}/>)
+            }
+          </ul>
+        </section>
       </main>
     </div>
   );
